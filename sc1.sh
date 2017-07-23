c03fdc175356db341287f947f25ef4d40c2a00c8
Dev Bharel <dbharel@hawk.iit.edu>
2017-07-23 09:21:34 -0500

    asdf

--- sc1.sh
+++ sc1.sh
@@ -1,4 +1,3 @@
-export qtumd-start="~/qtum/src/qtumd -regtest -server -rpcuser=root -rpcpassword=DREX" 
+export qtumd_start="~/qtum/src/qtumd -regtest -server -rpcuser=root -rpcpassword=DREX" 
 export qc="~/qtum/src/qtum-cli -regtest -rpcuser=root -rpcpassword=DREX"
-
 qc createcontract $(cat solartoken.bytecode) 10000000 #ADMIN created contract
